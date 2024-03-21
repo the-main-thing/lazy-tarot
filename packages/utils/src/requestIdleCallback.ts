bd2255115a57a93d622b9631c11e4dfce9a94ec4
Pavel Shevtsov <mail@pavelshevtsov.com>
2024-03-21 19:12:23 +0400

    fix: requestIdleCallback

--- packages/utils/src/requestIdleCallback.ts
+++ packages/utils/src/requestIdleCallback.ts
@@ -1,10 +1,6 @@
 export const requestIdleCallback = (callback: () => void) => {
-	const asTimeout = () => {
-		const id = setTimeout(callback, 0)
-		return () => clearTimeout(id)
-	}
 	try {
-		const id = window.requestIdleCallback(asTimeout)
+		const id = window.requestIdleCallback(callback)
 		return () => window.cancelIdleCallback(id)
 	} catch {
 		const id = setTimeout(callback, 0)
